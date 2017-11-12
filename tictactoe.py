9e56ed7156657b56c6f3255c05a988ef805f7a10
TKhare <tjskhare@gmail.com>
2017-11-12 07:43:54 -0500

    Game Finished

--- tictactoe.py
+++ tictactoe.py
@@ -23,94 +23,106 @@ empty_board = pygame.image.load('board.png')
 playerflag=True
 lastposition=False
 player_1 = 1
-plater_2 = 2
+player_2 = 2
 BLACK = (0,0,0)
+player_1_x = pygame.image.load('player_1_x.png')
+player_2_o = pygame.image.load('player_2_o.png')
 def winning(number):
         #displays 'stamina'
         Font1 = pygame.font.SysFont('monaco', 24)
         winSurface = Font1.render('Player {0} Wins'.format(number), True, BLACK)
         winRect = winSurface.get_rect()
-        winRect.midtop = (300, 10)
+        winRect.midtop = (300, 1)
         DISPLAYSURF.blit(winSurface,winRect)
 def check_winning():
-	if positions[0]==positions[1]==positions[2]:
-		if positions[0]==1:
-			winning(player_1)
-		if positions[0]==2:
-			winning(player_2)
-	if positions[3]==positions[4]==positions[5]:
-		if positions[3]==1:
-			winning(player_1)
-		if positions[3]==2:
-			winning(player_2)
-	if positions[6]==positions[7]==positions[8]:
-		if positions[6]==1:
-			winning(player_1)
-		if positions[6]==2:
-			winning(player_2)
-	if positions[0] == positions[4]==positions[8]:
-		if positions[0]==1:
-			winning(player_1)
-		if positions[0]==2:
-			winning(player_2)
-	if positions[2] == positions[4]== positions[6]:
-		if positions[2]==1:
-			winning(player_1)
-		if positions[2]==2:
-			winning(player_2)
-	if positions[0] == positions[3] == positions[6]:
-		if positions[0]==1:
-			winning(player_1)
-		if positions[0]==2:
-			winning(player_2)
-	if positions[1] == positions[4] == positions[7]:
-		if positions[1]==1:
-			winning(player_1)
-		if positions[1]==2:
-			winning(player_2)
-	if positions[2]== positions[5] == positions[8]:
-		if positions[2]==1:
-			winning(player_1)
-		if positions[2]==2:
-			winning(player_2)
+    if positions[0]==positions[1]==positions[2]:
+        if positions[0]==1:
+            winning(player_1)
+        if positions[0]==2:
+            winning(player_2)
+    if positions[3]==positions[4]==positions[5]:
+        if positions[3]==1:
+            winning(player_1)
+        if positions[3]==2:
+            winning(player_2)
+    if positions[6]==positions[7]==positions[8]:
+        if positions[6]==1:
+            winning(player_1)
+        if positions[6]==2:
+            winning(player_2)
+    if positions[0] == positions[4]==positions[8]:
+        if positions[0]==1:
+            winning(player_1)
+        if positions[0]==2:
+            winning(player_2)
+    if positions[2] == positions[4]== positions[6]:
+        if positions[2]==1:
+            winning(player_1)
+        if positions[2]==2:
+            winning(player_2)
+    if positions[0] == positions[3] == positions[6]:
+        if positions[0]==1:
+            winning(player_1)
+        if positions[0]==2:
+            winning(player_2)
+    if positions[1] == positions[4] == positions[7]:
+        if positions[1]==1:
+            winning(player_1)
+        if positions[1]==2:
+            winning(player_2)
+    if positions[2]== positions[5] == positions[8]:
+        if positions[2]==1:
+            winning(player_1)
+        if positions[2]==2:
+            winning(player_2)
 #Creating background board
 jk = 1
+DISPLAYSURF.fill([0,0,0])
+DISPLAYSURF.blit(empty_board, [0, 0])
 while True:
-    DISPLAYSURF.fill([0,0,0])
-    DISPLAYSURF.blit(empty_board, [0, 0])
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             (x, y)= pygame.mouse.get_pos()
             if playerflag == True:
-            	if jk == 1:
-#                if lastposition == False:
+                if jk == 1:
+                #if lastposition == False:
                     if x < 200 and y < 200 and positions[0] == None:
                         positions[0] = 1
                         playerflag = False
+                        DISPLAYSURF.blit(player_1_x, (10,10))
                     if x < 400 and y < 200 and x > 200 and positions[1] == None:
                         positions[1] = 1
                         playerflag = False
+                        DISPLAYSURF.blit(player_1_x, (210,10))
                     if x < 600 and y < 200 and x > 400 and positions[2] == None:
                         positions[2] = 1
                         playerflag = False
+                        DISPLAYSURF.blit(player_1_x, (410,10))
                     if x < 200 and y < 400 and y > 200 and positions[3] == None:
-                            positions[3] = 1
-                            playerflag = False
+                        positions[3] = 1
+                        playerflag = False
+                        DISPLAYSURF.blit(player_1_x, (10,210))
                     if x < 400 and y < 400 and x > 200 and y > 200 and positions[4] == None:
                         positions[4] = 1
                         playerflag = False
+                        DISPLAYSURF.blit(player_1_x, (210,210))
                     if x > 400 and y < 400 and y > 200 and positions[5] == None:
                         positions[5] = 1
                         playerflag = False
+                        DISPLAYSURF.blit(player_1_x, (410,210))
                     if x < 200 and y > 400 and positions[6] == None:
                         positions[6] = 1
                         playerflag = False
+                        DISPLAYSURF.blit(player_1_x, (10,410))
                     if x < 400 and y > 400 and x > 200 and positions[7] == None:
                         positions[7] = 1
                         playerflag = False
+                        DISPLAYSURF.blit(player_1_x, (210,410))
                     if x > 400 and y > 400 and positions[8] == None:
                         positions[8] = 1 
                         playerflag = False
+                        DISPLAYSURF.blit(player_1_x, (410,410))
+
                     lastpostion = True
 
             else:
@@ -118,38 +130,47 @@ while True:
                 if jk == 1:
                     if x < 200 and y < 200 and positions[0] == None:
                         positions[0] = 2
-                        playerflag = True   
+                        playerflag = True
+                        DISPLAYSURF.blit(player_2_o, (10,10))   
                     if x < 400 and y < 200 and x > 200 and positions[1] == None:
                         positions[1] = 2
                         playerflag = True
+                        DISPLAYSURF.blit(player_2_o, (210,10))
                     if x < 600 and y < 200 and x > 400 and positions[2] == None:
                         positions[2] = 2
                         playerflag = True
+                        DISPLAYSURF.blit(player_2_o, (410,10))
                     if x < 200 and y < 400 and y > 200 and positions[3] == None:
                         positions[3] = 2
                         playerflag = True
+                        DISPLAYSURF.blit(player_2_o, (10,210))
                     if x < 400 and y < 400 and x > 200 and y > 200 and positions[4] == None:
                         positions[4] = 2
                         playerflag = True
+                        DISPLAYSURF.blit(player_2_o, (210,210))
                     if x > 400 and y < 400 and y > 200 and positions[5] == None:
                         positions[5] = 2
                         playerflag = True
+                        DISPLAYSURF.blit(player_2_o, (410,210))
                     if x < 200 and y > 400 and positions[6] == None:
                         positions[6] = 2
                         playerflag = True
+                        DISPLAYSURF.blit(player_2_o, (10,410))
                     if x < 400 and y > 400 and x > 200 and positions[7] == None:
                         positions[7] = 2
                         playerflag = True
+                        DISPLAYSURF.blit(player_2_o, (210,410))
                     if x > 400 and y > 400 and positions[8] == None:
                         positions[8] = 2 
                         playerflag = True
+                        DISPLAYSURF.blit(player_2_o, (410,410))
                     lastposition = False
             print positions    
     #for place in positions:
-    	#if place == 1:
-    		#draw image 1
-    	#if place == 2:
-    		#draw image 2
+        #if place == 1:
+            #draw image 1
+        #if place == 2:
+            #draw image 2
     check_winning()
 
     pygame.display.flip()
